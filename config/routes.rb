cf5da31c8b69642c2045548b5eb324440242b330
miguelhhuerto <miguelhhuerto@gmail.com>
2023-07-28 14:46:27 +0800

    Added User Controller and Model, and Installed Devise and Phonelib Gems for User.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_060729) do
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "username", default: ""
+    t.integer "genre", default: 0
+    t.string "reset_password_token"
+    t.string "phone_number"
+    t.integer "coins"
+    t.float "total_deposit"
+    t.integer "children_members"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
